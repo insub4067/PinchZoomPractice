02332c5be07dc19dd2f616f1f96110ea908eab96
insub <is.kim@uniwaffle.com>
2023-12-12 15:16:11 +0900

    Initial Commit

---
+++ PinchZoomPractice/PinchZoomPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomPracticeApp.swift
+//  PinchZoomPractice
+//
+//  Created by insub on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
