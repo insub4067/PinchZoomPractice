4cefeb913fe6fd0bdc588e16ee2cd59261fb6a80
insub <vb15@kakao.com>
2023-12-12 15:24:18 +0900

    init

--- PinchZoomPractice/ContentView.swift
+++ PinchZoomPractice/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var scale: CGFloat = 1.0
+    @GestureState private var gestureScale: CGFloat = 1.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(.sample)
+            .resizable()
+            .scaledToFit()
+//            .frame(width: 200)
+            .frame(maxWidth: .infinity)
+            .scaleEffect(scale * gestureScale)
+            .gesture(
+                MagnificationGesture()
+                    .updating($gestureScale) { (value, gestureScale, _) in
+                        gestureScale = value
+                    }
+            )
     }
 }
 
